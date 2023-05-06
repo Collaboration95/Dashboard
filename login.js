b1c98d4f1e7c563e6284e07709933f439b88e2b7
Srividhya Gopal Application Admin <gopals1admin@autodesk.com>
2023-05-06 06:32:04 -0700

    c1

--- login.js
+++ login.js
@@ -2,21 +2,21 @@ const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const app = express();
-const table_name ={ login:"login" ,main:"regressionresult"};
+const table_name ={ login:"user_login" ,main:"regressionresult"};
 
 // Set up a MySQL connection
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Mcqueen@$95',
-    database: 'test_db'
+    password: 'admin',
+    database: 'test'
 });
 
 // Use middleware to parse HTTP POST request data
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Define a route to handle tzhe SQL query and return the results as JSON
+// Define a route to handle the SQL query and return the results as JSON
 app.post('/api/login', (req, res) => {
     const { username, password } = req.body;
     const sqlData = `SELECT * FROM ${table_name.login} WHERE username='${username}' AND password='${password}'`;
@@ -38,6 +38,27 @@ app.get('/api/sql-chart', (req, res) => {
     });
 });
 
+app.get('/api/sql-data1', (req, res) => {
+ 
+  const sqlData = 'SELECT testName, TestCategory, className, platform, job_name, job_buildnumber, installer_branch, installer_version, duration, startTime, endTime, outCome FROM  '+table_name.main + ' ORDER BY outcome';
+  
+    connection.query(sqlData, (error, dataResults, fields) => {
+      if (error) throw error;
+
+      //const header = headerResults.map(field => field.name);
+      const data = dataResults;
+
+      const response = {
+       // header,
+        data
+      };
+
+      res.json(response);
+    });
+  
+});
+
+// this funtion is replaced with sql-data1
 app.get('/api/sql-data', (req, res) => {
     const sqlHeader = 'SHOW COLUMNS FROM '+table_name.main;
     const sqlData = 'SELECT * FROM '+table_name.main;
@@ -129,7 +150,7 @@ app.get('/api/installer-versions/:installerVersion', (req, res) => {
     res.json(data);
   });
 });
-
+/*
 app.get('/api/latest-builds', (req, res) => {
     const sqlQuery = `
         SELECT *
@@ -148,7 +169,26 @@ app.get('/api/latest-builds', (req, res) => {
         const data = results;
         res.json(data);
     });
+});*/
+
+app.get('/api/latest-builds', (req, res) => {
+ const sqlQuery = `
+      SELECT t1.testName, t1.TestCategory, t1.className, t1.platform, t1.job_name, t1.job_buildnumber, t1.installer_branch, t1.installer_version, t1.duration, t1.startTime, t1.endTime, t1.outCome FROM ${table_name.main} t1
+      JOIN (
+          SELECT installer_branch, MAX(job_buildnumber) AS max_buildnumber
+          FROM ${table_name.main}
+          GROUP BY installer_branch
+      ) t2
+      ON t1.installer_branch = t2.installer_branch
+      AND t1.job_buildnumber = t2.max_buildnumber `;
+      console.log(sqlQuery);
+  connection.query(sqlQuery, (error, results, fields) => {
+      if (error) throw error;
+      const data = results;
+      res.json(data);
+  });
 });
+
 
 app.get('/api/longblob-data', (req, res) => {
   const id = req.query.id;
@@ -166,5 +206,5 @@ app.use(express.static('public'));
 
 // Start the server on port 3001 (or another port of your choice)
 app.listen(3000,'0.0.0.0', () => {
-    console.log('Server listening on port 3001');
+    console.log('Server listening on port 3000');
 });
--- public/homepage.js
+++ public/homepage.js
@@ -1,5 +1,5 @@
 sessionStorage.removeItem("loggedIn");
-const login = () => {
+function login() {
     const username = document.getElementById("Username").value;
     const password = document.getElementById("Password").value;
   
@@ -14,17 +14,32 @@ const login = () => {
     .then(data => {
       const login_verified = Object.keys(data).length;
       if (login_verified === 1) {
-        window.location.href = 'dashboard/MainPage.html';
+        //window.location.href = 'dashboard/mainpage';
+        window.location.href = 'dashboard/mainpage.html';
         sessionStorage.setItem("loggedIn", true);
       } else {
         alert('Invalid username or password');
       }
     })
     .catch(error => console.error(error));
-  };
+  }
   
 const loginButton = document.querySelector('#loginbuttondiv');
-loginButton.addEventListener('click',login);
+const loginInput = document.querySelector("#Password");
+
+loginButton.addEventListener('click',function(){
+  login();
+});
+
+loginInput.addEventListener("keydown",function(event){
+  if(event.key==="Enter")
+  {
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("login-button").click();
+   
+  }
+});
 
 
  
